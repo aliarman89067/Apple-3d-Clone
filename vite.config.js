27c803c069b7734c9ccd3193d7513b013efefc37
ali arman <fiver89067@gmail.com>
2024-03-22 13:49:02 +0500

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-0f",
-    project: "javascript-react"
-  })],
-
-  build: {
-    sourcemap: true
-  }
-})
+  plugins: [],
+});
